1b2f2de114034093ac4c8652683e64631a933bfb
timmy-quinn <tquinn5301@gmail.com>
2025-09-13 20:26:09 -0400

    added complete configuration

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-# 
+# Setup 
+### Windows installation
+Open a powershell terminal as an adminstrator
+
 ```
---winget install --id Git.Git -e --source winget
+winget install --id Git.Git -e --source winget
+
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+.\setup.ps1
 ```
--- setup.ps1
+++ setup.ps1
@@ -12,17 +12,37 @@ $chocoPackages = @(
     "nodejs.install" # used by nvim Mason/Lsp
 )
 
-git config --global alias.ch checkout
-git config --global alias.st status
-git config --global alias.cm commit -m
+$symlinks = @{
+    "$HOME\AppData\Local\nvim" = "NVim-Config"
+    "$ENV:PROGRAMFILES\WezTerm\wezterm_modules" = ".\wezterm\"
+}
 
+Set-Location $PSScriptRoot
+[Environment]::CurrentDirectory = $PSScriptRoot
+
+Write-Host "Installing packages..."
 foreach($package in $wingetPackages) {
     winget install -e --id $package
 }
 
 # Refresh environment to ensure choco is available
-$env:Path += ";$([Sustem.Environment]::GetEnvironmentVariable('ChocolateyInstall', 'Machine'))\bin"
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
 
 foreach($package in $chocoPackages) {
     choco install -y $package
 }
+
+
+
+Write-Host "Creating Symbolic Links..."
+foreach ($symlink in $symlinks.GetEnumerator()) {
+    Get-Item -Path $symlink.Key -ErrorAction SilentlyContinue | Remove-Item -Force -Recurse -ErrorAction SilentlyContinue
+    New-Item -ItemType SymbolicLink -Path $symlink.Key -Target (Resolve-Path $symlink.Value) -Force | Out-Null
+}
+
+# Configure git variables
+git config --global alias.ch checkout
+git config --global alias.st status
+git config --global alias.cm "commit -m"
+git config --global alias.pl pull
+git config --global alias.ph push
